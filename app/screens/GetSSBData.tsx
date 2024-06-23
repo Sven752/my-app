5cd4b550eb0ecc519ff178e90e58c3e77025cd79
sbergrat <sbergrat@smail.uni-koeln.de>
2024-06-23 23:26:36 +0200

    Added Pagination Dots

--- app/screens/GetSSBData.tsx
+++ app/screens/GetSSBData.tsx
@@ -1,28 +1,32 @@
-import React from "react";
-import { Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import React, { useState } from "react";
+import { Text, StyleSheet, ScrollView, Dimensions, View } from "react-native";
+import PaginationDot from "react-native-animated-pagination-dot";
 
 function GetSSBData() {
-  let xOffset: number = 0;
-  const calcPage = (xOffset: number): number => {
-    return 0;
+  const [page, setPage] = useState(0);
+  const updatePage = (xOffset: number): void => {
+    setPage(Math.floor(xOffset / Dimensions.get("window").width));
+    //console.log(Math.floor(xOffset / Dimensions.get("window").width));
   };
 
   return (
-    <ScrollView
-      style={styles.background}
-      horizontal={true}
-      pagingEnabled={true}
-      showsHorizontalScrollIndicator={true}
-      onScroll={(event) => {
-        xOffset = event.nativeEvent.contentOffset.x;
-        console.log(xOffset);
-      }}
-      scrollEventThrottle={16}
-    >
-      <Text style={styles.startButton}>Page 1</Text>
-      <Text style={styles.startButton}>Page 2</Text>
-      <Text style={styles.startButton}>Page 3</Text>
-    </ScrollView>
+    <View style={{ flex: 1 }}>
+      <ScrollView
+        style={styles.background}
+        horizontal={true}
+        pagingEnabled={true}
+        showsHorizontalScrollIndicator={true}
+        onScroll={(event) => {
+          updatePage(event.nativeEvent.contentOffset.x);
+        }}
+        scrollEventThrottle={16}
+      >
+        <Text style={styles.startButton}>Page 1</Text>
+        <Text style={styles.startButton}>Page 2</Text>
+        <Text style={styles.startButton}>Page 3</Text>
+      </ScrollView>
+      <PaginationDot activeDotColor={"black"} curPage={page} maxPage={3} />
+    </View>
   );
 }
 const styles = StyleSheet.create({
@@ -32,6 +36,9 @@ const styles = StyleSheet.create({
   startButton: {
     width: Dimensions.get("window").width,
     backgroundColor: "green",
+  },
+  paginationDots: {
+    height: 50,
   },
 });
 export default GetSSBData;
